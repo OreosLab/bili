e598b1443c56697a8d9368e3ca8fbb4cd7bf3c20
Oreo <62660101+Oreomeow@users.noreply.github.com>
2021-10-12 17:10:28 +0800

    :sparkles: 更新一键脚本并支持 elecV2P

--- bilibili_helper.sh
+++ bilibili_helper.sh
@@ -1,56 +1,67 @@
 #!/usr/bin/env bash
 #new Env('BILIBILI-HELPER');
 
-if ! [ -x "$(command -v java)" ]; then
-	echo "开始安装Java运行环境........."
-	apk update
-	apk add openjdk8
+if [ -f "$(dirname "$0")/utils_env.sh" ]; then
+	source "$(dirname "$0")/utils_env.sh"
+else
+	wget -q -O utils_env.sh https://ghproxy.com/https://raw.githubusercontent.com/Oreomeow/checkinpanel/master/utils_env.sh
+	source "$(dirname "$0")/utils_env.sh"
 fi
-if [ ! -d "/ql/scripts/bilibili/" ]; then
-	mkdir /ql/scripts/bilibili
+check_env
+check_jq_installed_status
+check_java_installed_status
+
+if [ "${pannel}" == "qinglong" ]; then
+	bili_path="/ql/scripts/bilibili"
+	conf_file="/ql/config/java_conf.json"
+elif [ "${pannel}" == "elecv2p" ]; then
+	bili_path="/usr/local/app/script/Shell/checkinpanel/bilibili"
+	conf_file="/usr/local/app/script/Lists/java_conf.json"
 fi
-cd bilibili
+
+if [ ! -d "${bili_path}" ]; then
+	mkdir -p "${bili_path}"
+fi
+
+cd "${bili_path}" || exit
 if [ -f "/tmp/bili-helper.log" ]; then
 	VERSION=$(grep "当前版本" "/tmp/bili-helper.log" | awk '{print $2}')
 else
 	VERSION="0"
 fi
 echo "当前版本:"$VERSION
-latest=$(curl -s https://api.github.com/repos/JunzhouLiu/BILIBILI-HELPER-PRE/releases/latest)
 
-latest_VERSION=$(echo $latest | jq '.tag_name' | sed 's/v\|"//g')
-echo "最新版本:"$latest_VERSION
-download_url=$(echo $latest | jq '.assets[0].browser_download_url' | sed 's/"//g')
+latest=$(curl -s https://api.github.com/repos/Oreomeow/bili/releases/latest)
+latest_VERSION=$(echo "$latest" | jq '.tag_name' | sed 's/v\|"//g')
+echo "最新版本:""$latest_VERSION"
+download_url=$(echo "$latest" | jq '.assets[0].browser_download_url' | sed 's/"//g')
 download() {
 	curl -L -o "./BILIBILI-HELPER.zip" "https://ghproxy.com/$download_url"
 	mkdir ./tmp
 	echo "正在解压文件......."
 	unzip -o -d ./tmp/ BILIBILI-HELPER.zip
 	cp -f ./tmp/BILIBILI-HELPER*.jar BILIBILI-HELPER.jar
-	if [ ! -f "/ql/config/config.json" ]; then
+	if [ ! -f "${conf_file}" ]; then
 		echo "配置文件不存在。"
-		cp -f ./tmp/config.json /ql/config/config.json
+		cp -f ./tmp/config.json "${conf_file}"
 	fi
 	echo "清除缓存........."
 	rm -rf tmp
 	rm -rf BILIBILI-HELPER.zip
 	echo "更新完成"
 }
+
 function version_lt() { test "$(echo "$@" | tr " " "\n" | sort -rV | head -n 1)" != "$1"; }
-if version_lt $VERSION $latest_VERSION; then
+if version_lt $VERSION "$latest_VERSION"; then
 	echo "有新版本，开始更新"
 	download
 else
 	echo "已经是最新版本，不需要更新！！！"
 fi
-if [ ! -f "/ql/scripts/bilibili/BILIBILI-HELPER.jar" ]; then
+if [ ! -f "${bili_path}/BILIBILI-HELPER.jar" ]; then
 	echo "没找到BILIBILI-HELPER.jar，开始下载.........."
 	download
 fi
-files=$(ls /ql/config/*.json)
-for file_name in $files; do
-	if [[ $file_name != *auth* ]]; then
-		echo "配置文件路径:"$file_name
-		java -jar BILIBILI-HELPER.jar $file_name
-	fi
-done
+
+echo "配置文件路径:""$conf_file"
+java -jar BILIBILI-HELPER.jar "$conf_file"
